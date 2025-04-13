1b0433d0e05c118c0871aef6ec6a4f23495860dc
Jowen <diezjowenmiguel@gmail.com>
2025-04-13 13:28:02 +0800

    push

--- frontend/src/Pages/Signin.jsx
+++ frontend/src/Pages/Signin.jsx
@@ -14,7 +14,7 @@ export const Signin = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-<<<<<<< HEAD
+
     if (!email || !password) {
       setError("Please fill in all fields.");
       return;
@@ -48,35 +48,8 @@ export const Signin = () => {
         setError("Invalid email or password.");
       }finally {
         setLoading(true); 
-=======
-    try {
-      const response = await fetch("http://localhost:8000/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Login failed");
->>>>>>> renz-front-end
       }
-
-      // Optional: extract data if needed
-      const data = await response.json();
-      console.log("Login successful:", data);
-
-      // Navigate to the dashboard after successful login
-      navigate("/dashboard");
-    } catch (err) {
-      console.error("Login failed:", err);
-      setError("Invalid email or password.");
     }
-  };
 
   return (
     <div className="min-h-screen flex text-gray-900 font-sans">
@@ -119,11 +92,9 @@ export const Signin = () => {
             type="email"
             placeholder="Email"
             value={email}
-<<<<<<< HEAD
+
             onChange={(e) => setEmail(e.target.value) }
-=======
-            onChange={(e) => setEmail(e.target.value)}
->>>>>>> renz-front-end
+
             className="w-full border rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
 
@@ -157,7 +128,7 @@ export const Signin = () => {
         </form>
       </div>
     </div>
-  );
+  )
 };
 
 export default Signin;
